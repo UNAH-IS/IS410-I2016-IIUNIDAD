eabe9afe4f5067b0e2e07dbfed526f7b28c54a7f
Erick Reyes <erick.marin.v@gmail.com>
2016-03-28 10:57:28 -0600

    Actualizacion ejercicio playstore

--- EjercicioPlayStore/src/application/ControladorAplicaciones.java
+++ EjercicioPlayStore/src/application/ControladorAplicaciones.java
@@ -9,27 +9,45 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import modelo.Categoria;
-import utilidades.UtilidadConexion;
+import modelo.Desarrollador;
+import modelo.Empresa;
+import utilidades.GestorConexiones;
 
 public class ControladorAplicaciones implements Initializable{
 	
-	private UtilidadConexion utilidadConexion;
+	private GestorConexiones gestorConexiones;
 	@FXML private ComboBox<Categoria> cboCategorias;
+	@FXML private ComboBox<Desarrollador> cboDesarrolladores;
+	@FXML private ComboBox<Empresa> cboEmpresas;
 	
 	
 	private ObservableList<Categoria> categorias;
+	private ObservableList<Desarrollador> desarrolladores;
+	private ObservableList<Empresa> empresas;
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		utilidadConexion = new UtilidadConexion();
-		utilidadConexion.establecerConexion();
+		gestorConexiones = new GestorConexiones();
+		gestorConexiones.establecerConexion();
 		categorias = FXCollections.observableArrayList();
+		desarrolladores = FXCollections.observableArrayList();
+		empresas= FXCollections.observableArrayList();
 		cboCategorias.setItems(categorias);
+		cboDesarrolladores.setItems(desarrolladores);
+		cboEmpresas.setItems(empresas);
 		inicializarInformacion();
+		gestorConexiones.cerrarConexion();
 	}
 	
 	public void inicializarInformacion(){
-		categorias.add(new Categoria(1,"Prueba", "Descripcion Prueba"));
+		Categoria.llenarListaCategorias(
+				categorias, gestorConexiones.getConexion()
+		);
+		Desarrollador.llenarListaDesarrolladores(
+			desarrolladores, gestorConexiones.getConexion()
+		);
+		Empresa.llenarListaEmpresas(
+			empresas, gestorConexiones.getConexion()
+		);
+		
 	}
-	
-	
 }
--- EjercicioPlayStore/src/modelo/Categoria.java
+++ EjercicioPlayStore/src/modelo/Categoria.java
@@ -1,5 +1,12 @@
 package modelo;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.collections.ObservableList;
+
 public class Categoria {
 	private int codigoCategoria;
 	private String nombreCategoria;
@@ -33,5 +40,34 @@ public class Categoria {
 	@Override
 	public String toString() {
 		return nombreCategoria;
+	}
+	
+	public static void llenarListaCategorias(
+				ObservableList<Categoria> lista,
+				Connection conexion
+			){
+		//Consultar informacion de categorias
+		try {
+			Statement instruccion = conexion.createStatement();
+			ResultSet resultado = instruccion.executeQuery("SELECT codigo_categoria, "
+					+ "nombre_categoria, descripcion "
+					+ "FROM tbl_categorias");
+			
+			while(resultado.next()){
+				lista.add(
+						new Categoria(
+							resultado.getInt("codigo_categoria"),
+							resultado.getString("nombre_categoria"),
+							resultado.getString("descripcion")
+						)
+				);
+			}
+			
+			instruccion.close();
+			resultado.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		//Llenar Lista
 	}
 }
